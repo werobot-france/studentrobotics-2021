19045fff50913fca64a2101d1e410cc1557fba1e
Xa-a <harounkrifa35@gmail.com>
2021-01-24 15:28:06 +0100

    ajout d'orientation avant de se déplacer plus tentative de retour en arrière

--- robot.py
+++ robot.py
@@ -12,8 +12,14 @@ class Monrobot(Robot):
     pillarName=''
     targetOwner=''
     targetDistance=0
+<<<<<<< Updated upstream
     positionThreshold= 0.1
     rotationThreshold = 12
+=======
+    lastTarget=''
+    innerWestWallCoef = (0.528,1.2)
+    innerEastWallCoef=(-0.528,1.2)
+>>>>>>> Stashed changes
     upperPillars=['PN', 'EY', 'PO', 'YL']
     midlePillar=['BE']
     lowerPillars=['BG', 'OX', 'TS', 'VB', 'HV', 'BN', 'SW', 'SZ']
@@ -108,6 +114,7 @@ class Monrobot(Robot):
             self.theta = self.toPiPi(tempList[2].bearing + atan2(-(P[1]-self.y),P[0]-self.x))
             self.actu = self.time()
         self.age = self.time()-self.actu
+<<<<<<< Updated upstream
 
         for tx in transmitters:
             if tx.signal_strength > self.strongestSignal and not tx.target_info.owned_by == self.zone:
@@ -115,6 +122,46 @@ class Monrobot(Robot):
                 self.targetBearing = tx.bearing
                 self.pillarName = tx.target_info.station_code
                 self.targetOwner = tx.target_info.owned_by
+=======
+        if self.targetPillar == None :
+            self.selectTargets()
+        else :
+            tempPillar = [tx for tx in self.transmitters if tx.target_info.station_code == self.targetPillar.target_info.station_code]
+            if len(tempPillar) > 0:
+                tempPillar = tempPillar[0]
+                if tempPillar.target_info.owned_by == self.zone :
+                    logging.debug(f"Ancienne cible {self.targetPillar.target_info.station_code} = {tempPillar.target_info.station_code} deja a moi. Changement.")
+                    self.selectTargets()
+                else :
+                    self.targetPillar = tempPillar
+            else :
+                self.selectTargets()
+        self.wayPointBearing = self.toPiPi(self.theta - (atan2(-(self.targetWayPoint[1]-self.y),self.targetWayPoint[0]-self.x)))
+        logging.info(f" Info sur les cibles :")
+        logging.info(f"-Target Pillar : {self.pillarName}")
+        logging.info(f"-Target Way Point : {self.targetWayPoint}")
+        logging.info(f"-Way Point Bearing : {self.wayPointBearing}")
+
+
+    def selectTargets(self):
+        if len(self.transmitters) == 0 : 
+            self.targetPillar = None
+            self.targetWayPoint = (0,0)
+            return
+        if self.lastTarget=='EY':
+            self.pillarName = 'AUCUN'
+            self.targetPillar = None
+            self.targetWayPoint = (-4.37,-1.58)
+            return
+        for tx in self.transmitters:
+            logging.debug(f"Evaluation 1ere cond dans selctT : {(not tx.target_info.owned_by == self.zone)} pour {tx.target_info.station_code}")
+            if  (not tx.target_info.owned_by == self.zone) and self.isTargetReachable(tx):
+                self.targetPillar = tx
+                self.pillarName = tx.target_info.station_code
+                self.targetWayPoint = self.wayPoints[self.pillarName]
+                print(self.targetPillar)
+                return
+>>>>>>> Stashed changes
             self.sleep(0.1)
 
 
@@ -134,6 +181,7 @@ class Monrobot(Robot):
         self.leftMotor.power = l
         self.rightMotor.power = r
 
+<<<<<<< Updated upstream
 
 
 
@@ -195,9 +243,87 @@ def reachFirstPillar():
     R.setMotors(0,0)
     R.radio.claim_territory()
 reachFirstPillar()
+=======
+    def orientToTarget (self):
+        logging.debug("Entree dans orientToTarget")
+        while -pi/4 < self.wayPointBearing > pi/4 :
+            R.update()
+            if self.wayPointBearing > 0 :
+                self.setMotors(21*(self.wayPointBearing),-21*(self.wayPointBearing))
+                logging.debug(f"moteur gauche : {self.leftMotor.power} moteur droit : {self.rightMotor.power}")
+
+            elif self.wayPointBearing < 0 :
+                self.setMotors(21*(self.wayPointBearing),-21*(self.wayPointBearing))
+                logging.debug(f"moteur gauche : {self.leftMotor.power} moteur droit : {self.rightMotor.power}")
+            self.sleep(0.1)
+        return
+    def claim(self):
+        self.lastTarget=self.targetPillar.target_info.station_code
+        self.radio.claim_territory()
+
+    def goToTarget(self):
+        logging.debug("Entree dans goToTarget")
+        try :
+            dist = sqrt(1/self.targetPillar.signal_strength)
+            logging.debug(f"Distance au pillar {self.targetPillar.target_info.station_code} : {dist}")
+        except :
+            logging.debug("Distance au pillier non calculable, distance fixee à 3m")
+            dist = 3
+        
+        if  dist < 0.48 :
+            self.setMotors(0,0)
+            logging.debug("Arret et claim")
+            self.claim()
+        elif self.tsAV :
+            logging.debug("Coince a l avant : je recule")
+            if self.wayPointBearing > 0 :
+                logging.debug("Nez vers la droite")
+                self.setMotors(-100*cos(self.wayPointBearing),-100)
+            else :
+                logging.debug("Nez vers la gauche")
+                self.setMotors(-100,-100*cos(self.wayPointBearing))
+            self.sleep(0.2)
+        elif self.wayPointBearing > 0 :
+            logging.debug("Bifurque à droite")
+            self.setMotors(speed,speed*cos(self.wayPointBearing)**3)
+            
+        elif self.wayPointBearing < 0 :
+            logging.debug("Bifurque à gauche")            
+            self.setMotors(speed*cos(self.wayPointBearing)**3,speed)         
+        logging.debug(f"moteur gauche : {self.leftMotor.power} moteur droit : {self.rightMotor.power}")
+
+
+        
+       
+R = Monrobot()
+speed = 70
+delay = 0.1
+
+def reachFirstPillar(R):
+    dist = 1
+    firstPillar = R.transmitters[0]
+    if R.zone==0: R.setMotors(79,89)
+    else : R.setMotors(89,79)
+    while dist > 0.48 :
+        R.sleep(0.1)
+        R.update()
+        firstPillar = [tx for tx in R.transmitters if tx.target_info.station_code == firstPillar.target_info.station_code][0]
+        dist = sqrt(1/firstPillar.signal_strength)
+        logging.debug(f"Distance au 1st pillar {firstPillar.target_info.station_code} : {dist}")
+    R.setMotors(0,0)
+    R.claim()
+    logging.debug("first pillar should be claimed")
+
+
+reachFirstPillar(R)
+>>>>>>> Stashed changes
 R.setMotors(speed,speed)
 while True :
     R.update()
+<<<<<<< Updated upstream
     R.lookToTarget()
+=======
+    R.orientToTarget()
+>>>>>>> Stashed changes
     R.goToTarget()
     R.sleep(delay)
